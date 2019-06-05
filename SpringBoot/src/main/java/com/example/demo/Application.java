ec2ae0a46da02c8bccb07564b65711ed10344350
Rajesh <glakshmirajesh256@gmail.com>
2019-06-05 21:02:03 +0530

    test

--- SpringBoot/src/main/java/com/example/demo/Application.java
+++ SpringBoot/src/main/java/com/example/demo/Application.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.dao.CityDaoImpl;
+import com.example.model.City;
+
 @SpringBootApplication
 public class Application {
+	
+	 static CityDaoImpl cityDaoImpl=new CityDaoImpl();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("Test1");
+		System.out.println("Test2");
+		List<City> city=cityDaoImpl.getAllCities();
 	}
 }
